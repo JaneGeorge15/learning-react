4ae8c608b85af13ae0ac07ac40c9a53c77958742
janegeorge15 <janegeorge15@gmail.com>
2021-02-16 21:02:48 +0530

    react router on click

--- src/components/TaskTracker.js
+++ src/components/TaskTracker.js
@@ -1,54 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
 
-const TaskTracker = () => {
+const TaskTracker = ({match}) => {
     return(
-        <h1>Helppppp task tracker</h1>
+        <h1>Welcome {match? match.params.username : '...'}</h1>
     )
 }
 
 export default TaskTracker
 
-
-
-
-
-
-
-// import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
-// import TaskTracker from "./TaskTracker";
-
-// export default function BasicExample() {
-//   return (
-//     <Router>
-//       <div>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/task-tracker">Task Tracker</Link>
-//           </li>
-//         </ul>
-
-//         <hr />
-
-//         <Switch>
-//           <Route exact path="/">
-//             <TaskTracker />
-//           </Route>
-//           <Route path="/task-tracker">
-//             <TaskTracker />
-//           </Route>
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
 
